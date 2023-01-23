edeaad8551ceddf32e24b759a0c87ea3e7878ef7
marijkevanhell <jukevanhell@gmail.com>
2023-01-23 18:05:47 +0000

    Initial commit with all my js work so far

---
+++ for-loop.js
@@ -0,0 +1,8 @@
+let total = 0
+let limit = 10
+for (let i = 0; i < 10; i++) {  
+  total += i 
+  // log the numbers 0 through 9  
+  console.log(total)  
+}  
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,3 @@
+const pets = ['cat', 'dog', 'rat']
+pets[i] = pets[i] + 's'
+console.log(pets)
